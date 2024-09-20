bf9585ed15503914711280f09fc35831f9f287b0
santhosh <144583697+santhoshofcl@users.noreply.github.com>
2024-09-21 03:18:09 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -36,7 +36,7 @@ function checkDesktopMode() {
   const content = document.getElementById('content');
 
   // Toggle visibility based on screen width
-  if (screenWidth < 1025) {
+  if (screenWidth < 1023) {
     modal.style.display = 'block';  // Show the modal
     content.style.display = 'none'; // Hide the website content
   } else {
